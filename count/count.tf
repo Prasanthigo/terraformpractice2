8033fb427495c74a71dc7cc13cbcae2d901b53f5
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2024-10-05 23:50:48 -0500

    added

--- count/count.tf
+++ count/count.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "conditions " {
+resource "aws_instance" "conditions" {
     count = 7
     ami = var.ami_id
     instance_type = var.instance_names[count.index] == "MongoDB" || var.instance_names[count.index] == "Mysql" ? "t3.medium" : "t2.micro"
