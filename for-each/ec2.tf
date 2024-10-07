ea775f8d5e198b032399e4608e8226cbd1e03134
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2024-10-07 12:28:27 -0500

    added for-each

--- for-each/ec2.tf
+++ for-each/ec2.tf
@@ -7,6 +7,15 @@ resource "aws_instance" "roboshop" {
     }
 }
 
-output "aws_instance_info" {
-    value = aws_instance.roboshop
+# output "aws_instance_info" {
+#     value = aws_instance.roboshop
+# }
+resource "aws_route53_record" "www" {
+    for_each = var.instances
+    zone_id = var.zone_id
+    name = "${each.key}.hetvik.online"
+    type = "A"
+    ttl = 1
+    records = [each.key == "payment" ? each.value.public_ip : each.value.private_ip]
 }
+
