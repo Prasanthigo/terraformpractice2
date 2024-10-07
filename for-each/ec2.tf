4d11c212a1ffb51ff536a9e67fa33a640a5aeed1
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2024-10-07 12:00:42 -0500

    added for-each

--- for-each/ec2.tf
+++ for-each/ec2.tf
@@ -1,6 +1,10 @@
 resource "aws_instance" "roboshop" {
+    for_each = var.instances
     ami = var.ami_id
-    instance_type = "t2.micro"
+    instance_type = each.value
+    tags = {
+        Name = each.key
+    }
 }
 
 output "aws_instance_info" {
--- for-each/variables.tf
+++ for-each/variables.tf
@@ -3,3 +3,15 @@ variable "ami_id"{
     default = "ami-0b4f379183e5706b9"
 }
 
+variable "instances" {
+    type = map
+    default = {
+        MongoDB = "t3.medium"
+        MYSQL = "t3.medium"
+        Redis = "t2.micro"
+        RabbitMQ = "t2.micro"
+        Catalogue = "t2.micro"
+        Payment = "t2.micro"
+    }
+
+}
