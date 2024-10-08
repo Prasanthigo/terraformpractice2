7bdbed74da38fe5cb4e134c964ac936e47e84042
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2024-10-08 12:34:27 -0500

    state

--- for-each/provider.tf
+++ for-each/provider.tf
@@ -5,6 +5,13 @@ terraform {
         version = "5.15.0"
     }
   }
+
+  backend "s3" {
+    bucket = "roboshop1-bucket"
+    key = "foreach"
+    region = "us-east-1"
+    dynamodb_table = "roboshop_locking"
+  }
 }
  provider "aws" {
     region = "us-east-1"
