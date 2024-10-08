9859507a64874d10d82238c6f18bbf6f83b37211
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2024-10-08 13:40:28 -0500

    tfvars

--- tfvars/provider.tf
+++ tfvars/provider.tf
@@ -1,15 +1,19 @@
 terraform {
-    required_providers = {
-        aws = {
-            source = "hashicorp/aws"
-            version = "5.15.0"
-        }     
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.15.0"
     }
-    backend "s3"{
-        bucket = "roboshop1-bucket"
-        key = "tfvars"
-        region = "us-east-1"
-        dynamodb_table = "roboshop-locking"
-    }
+  }
 
+  backend "s3" {
+    bucket = "roboshop1-bucket"
+    key = "foreach"
+    region = "us-east-1"
+    dynamodb_table = "roboshop-locking"
+  }
 }
+ provider "aws" {
+    region = "us-east-1"
+
+ }
