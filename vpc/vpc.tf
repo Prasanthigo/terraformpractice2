d415a6dd0eb132eb7c2e699a37009334dd5492dc
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2024-10-13 11:43:05 -0500

    adding vpc

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -95,6 +95,7 @@ resource "aws_instance" "ec2" {
     instance_type = "t2.micro"
     subnet_id = aws_subnet.public.id
     security_groups = [aws_security_group.allow_http_ssh.name]
+    associate_public_ip_address = true
     tags = {
         Name = "web"
     }
