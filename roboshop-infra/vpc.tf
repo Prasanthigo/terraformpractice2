4348729ec464df70fd54d643cc3ce8ac449e7868
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2024-10-13 13:15:47 -0500

    adding vpc

--- roboshop-infra/vpc.tf
+++ roboshop-infra/vpc.tf
@@ -1,7 +1,6 @@
 module "this" {
     source = "../terraform-aws-vpc"
     cidr_block = var.cidr_block
-    common_tags = var.common_tags
-    vpc_tags = var.vpc_tags
+    
     tags = merge(var.common_tags, var.vpc_tags)
 }
