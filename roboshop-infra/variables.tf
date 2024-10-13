e4dc86a83ea7f0417a83448302a5621dd7380c21
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2024-10-13 13:01:26 -0500

    adding vpc

--- roboshop-infra/variables.tf
+++ roboshop-infra/variables.tf
@@ -2,10 +2,17 @@ variable "cidr_block" {
     default = "10.0.0.0/16"
 }
 
-variable "tags" {
+variable "common_tags" {
     default = {
-        Name = "Roboshop"
+        Project = "Roboshop"
         Env = "DEV"
         Terraform = true
+    }
+}
+
+variable "vpc_tags" {
+    default = {
+        Name = "Roboshop"
+        
     }
 }
