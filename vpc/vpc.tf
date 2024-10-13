4b4d8e8efcdc9d041d43901d8e20f71b8ed01da4
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2024-10-13 10:14:43 -0500

    adding vpc

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -6,3 +6,19 @@ resource "aws_vpc" "main" {
         terraform = true
     }
 }
+
+resource "aws_subnet" "public" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+        Name = "Roboshop-public"
+    }
+}
+
+resource "aws_subnet" "private" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.2.0/24"
+    tags = {
+        Name = "Roboshop-private"
+    }
+}
