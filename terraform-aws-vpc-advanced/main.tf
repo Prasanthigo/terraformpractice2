923d78258c2632aa03ed8319b433168ed9d17d51
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2024-11-09 13:18:10 -0600

    modified

--- terraform-aws-vpc-advanced/main.tf
+++ terraform-aws-vpc-advanced/main.tf
@@ -86,11 +86,11 @@ resource "aws_subnet" "public" {
     )
  }
 
- resource "aws_eip" "lb" {
+ resource "aws_eip" "eip" {
   domain   = "vpc"
 }
 resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.main.id
+  allocation_id = aws_eip.eip.id
   subnet_id     = aws_subnet.public[0].id
 
   tags = merge(
