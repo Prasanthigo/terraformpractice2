bef0ee0f260c784619c6f1306ffd58f318961157
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2024-11-26 00:09:59 -0600

    mod

--- roboshop-infra-standard/01-vpc/parameters.tf
+++ roboshop-infra-standard/01-vpc/parameters.tf
@@ -8,8 +8,8 @@ resource "aws_ssm_parameter" "vpc_id" {
 
 resource "aws_ssm_parameter" "public_subnet_ids" {
     name = "/${var.project_name}/${var.env}/public_subnet_ids"
-    type = "String"
-    value = module.vpc.public_subnet_ids # module should have output decalration
+    type = "StringList"
+    value = join(",",module.vpc.public_subnet_ids) # module should have output decalration
 
 
 }
@@ -17,13 +17,13 @@ resource "aws_ssm_parameter" "public_subnet_ids" {
 resource "aws_ssm_parameter" "private_subnet_ids" {
     name = "/${var.project_name}/${var.env}/private_subnet_ids"
     type = "String"
-    value = module.vpc.private_subnet_ids # module should have output decalration
+    value = join(",",module.vpc.private_subnet_ids) # module should have output decalration
 
 }
 
 resource "aws_ssm_parameter" "database_subnet_ids" {
     name = "/${var.project_name}/${var.env}/database_subnet_ids"
     type = "String"
-    value = module.vpc.database_subnet_ids # module should have output decalration
+    value = join(",",module.vpc.database_subnet_ids) # module should have output decalration
 
 }
